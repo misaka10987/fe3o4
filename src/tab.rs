3060ec503d9c951d23e93341044dc92fad1d10c4
misaka10987 <misaka10987@outlook.com>
2024-09-12 20:26:13 +0800

    fix macro

--- src/tab.rs
+++ src/tab.rs
@@ -30,8 +30,8 @@ pub use lazy_static;
 #[macro_export]
 macro_rules! def_regtab {
     ($t:ty,$i:ident) => {
-        $crate::reg::tab::lazy_static::lazy_static! {
-            pub static ref $i: $crate::RegTab<$t> = $crate::RegTab($crate::reg::tab::dashmap::DashMap::new());
+        $crate::tab::lazy_static::lazy_static! {
+            pub static ref $i: $crate::RegTab<$t> = $crate::RegTab($crate::tab::dashmap::DashMap::new());
         }
         impl $crate::HasRegTab for $t {
             fn reg_rab() -> &'static $crate::RegTab<Self> {
